4de7cbda232ab885028e27486bf935676abad14f
lambobe <147500262+lambobe@users.noreply.github.com>
2024-12-12 00:14:18 +0800

    dec 12

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<link rel="icon" type="image/png" sizes="32x32" href="image/uclogo.png">
+<link rel="icon" type="image/png" sizes="32x32" href="image/uclogo.png" class="titlelogo">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -23,6 +23,7 @@
             background-color: #0c76e0;
             position: relative;
         }
+       
 
         .container {
             position: absolute;
